74aed49f32653bd3be4f22fe00c7b0d38353e942
Ray <rayrinaldy@gmail.com>
2017-04-03 11:49:49 +0800

    change the config

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
-var config = require('./config');
-var locals = { config: config };
+var config = require('./config/config');
+// var locals = { config: config };
 
 var port = process.env.PORT || 3000;
 
@@ -13,7 +13,9 @@ app.set('views', path.join(__dirname,'/app/pug'));
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
-	res.render('index', locals);
+	res.render('index', {
+		config : config,
+	});
 });
 
 app.listen(port, function(){
